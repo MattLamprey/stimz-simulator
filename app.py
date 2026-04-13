fd5924ca3a0d590fc91becf94673fda708875972
MattLamprey <49204376+MattLamprey@users.noreply.github.com>
2026-04-13 13:28:57 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -766,9 +766,9 @@ def score_product_type(row):
     intensity_need = predicted_stims["Intenseinputspikypain"] + predicted_stims["Weightedpressure"]
     visual_need = predicted_stims["Lookingatcolourormovement"]
 
-    if visual_need < 0.12:
-    product_types = product_types[
-        product_types["Product type"].str.strip() != "Visual stim"
+	if visual_need < 0.12:
+    		product_types = product_types[
+        		product_types["Product type"].str.strip() != "Visual stim"
     ].reset_index(drop=True)
 
     movement_need = (
